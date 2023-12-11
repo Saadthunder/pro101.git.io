f2004d141f14ee3cd4920ae22b3dd4fd4505bc77
Saadthunder <84610882+Saadthunder@users.noreply.github.com>
2023-12-11 10:01:11 +0600

    Add files via upload

---
+++ p101/rolldice.py
@@ -0,0 +1,23 @@
+import random
+response = input("press 'y' to roll a dice")
+ 
+def countdown():
+      
+     
+    if(response =="y"):
+        num=random.randint(1,6)
+        print(num)
+
+
+    
+        
+
+        
+        
+
+countdown()
+
+
+
+
+       
